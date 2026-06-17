817de69ef6333174ce71024e6d0a938e7d759b0b
EmilianoJMR <emiliano.mendozar.dev@gmail.com>
2026-06-17 13:53:35 -0600

    Añadiendo Productos Exterior

--- scripts/productos.js
+++ scripts/productos.js
@@ -72,3 +72,92 @@ const plantasInterior = [
         riego: "Cada 7 días"
     }
 ];
+
+const plantasExterior = [
+
+    {
+    id: 1,
+    nombre: "Lavandula spp.",
+    nombreComun: "Lavanda",
+    luz: "Sol directo",
+    riego: "1 vez cada 1 o 2 semanas",
+    imagen: "https://images.pexels.com/photos/12970054/pexels-photo-12970054.jpeg"
+  },
+  {
+    id: 2,
+    nombre: "Aloe barbadensis",
+    nombreComun: "Sábila / Aloe Vera",
+    luz: "Sol directo o resolana",
+    riego: "1 vez cada 2 o 3 semanas",
+    imagen: "https://images.pexels.com/photos/34465158/pexels-photo-34465158.jpeg"
+  },
+  {
+    id: 3,
+    nombre: "Bougainvillea spp.",
+    nombreComun: "Bugambilia",
+    luz: "Sol directo",
+    riego: "1 o 2 veces por semana",
+    imagen: "https://images.pexels.com/photos/16100361/pexels-photo-16100361.jpeg"
+  },
+  {
+    id: 4,
+    nombre: "Citrus x limon",
+    nombreComun: "Limonero / Árbol de Limón",
+    luz: "Sol directo",
+    riego: "2 a 3 veces por semana",
+    imagen: "https://images.pexels.com/photos/36977032/pexels-photo-36977032.jpeg"
+  },
+
+  {
+    id: 5,
+    nombre: "Pachypodium lamerei",
+    nombreComun: "Palma de Madagascar",
+    luz: "Sol directo",
+    riego: "1 vez cada 2 o 3 semanas",
+    imagen: "https://images.pexels.com/photos/31964234/pexels-photo-31964234.jpeg"
+  },
+  
+  {
+    id: 6,
+    nombre: "Nephrolepis exaltata",
+    nombreComun: "Helecho",
+    luz: "Sombra o semisombra",
+    riego: "2 a 3 veces por semana",
+    imagen: "https://images.pexels.com/photos/10903254/pexels-photo-10903254.jpeg"
+  },
+  
+  {
+    id: 7,
+    nombre: "Spathiphyllum wallisii",
+    nombreComun: "Cuna de Moisés",
+    luz: "Sombra o semisombra",
+    riego: "2 a 3 veces por semana",
+    imagen: "https://images.pexels.com/photos/19404633/pexels-photo-19404633.jpeg"
+  },
+  {
+    id: 8,
+    nombre: "Beaucarnea recurvata",
+    nombreComun: "Pata de Elefante",
+    luz: "Sol o semisombra",
+    riego: "1 vez cada 2 o 3 semanas",
+    imagen: "https://images.pexels.com/photos/30343682/pexels-photo-30343682.jpeg"
+  },
+  {
+    id: 9,
+    nombre: "Helianthus annuus",
+    nombreComun: "Girasol",
+    luz: "Sol directo",
+    riego: "3 a 4 veces por semana (casi diario en calor)",
+    imagen: "https://images.pexels.com/photos/14876097/pexels-photo-14876097.jpeg"
+  },
+  
+  {
+    id: 10,
+    nombre: "Jasminum sambac",
+    nombreComun: "Jazmín Árabe",
+    luz: "Sol directo",
+    riego: "2 a 3 veces por semana",
+    imagen: "https://images.pexels.com/photos/37745598/pexels-photo-37745598.jpeg"
+  }
+
+];
