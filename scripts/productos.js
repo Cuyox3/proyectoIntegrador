84c1e47e4e93696d4b6d81e9369f9009fb5428f3
luisSoto <luisalbertoramirezsoto2@gmail.com>
2026-06-17 13:07:58 -0600

    se crea js de productos y se agrega primer array con sus 10 objetos de plantas de interior y se vuelve a subir js scripts

---
+++ scripts/productos.js
@@ -0,0 +1,74 @@
+//Plantas de interior
+
+const plantasInterior = [
+    {
+        id: 1,
+        nombre: "Monstera Deliciosa",
+        nombreComun: "Costilla de Adán",
+        luz: "Indirecta brillante",
+        riego: "1 vez por semana"
+    },
+    {
+        id: 2,
+        nombre: "Sansevieria Trifasciata",
+        nombreComun: "Lengua de Suegra",
+        luz: "Baja a alta",
+        riego: "Cada 2 semanas"
+    },
+    {
+        id: 3,
+        nombre: "Epipremnum Aureum",
+        nombreComun: "Poto",
+        luz: "Indirecta media",
+        riego: "1 vez por semana"
+    },
+    {
+        id: 4,
+        nombre: "Spathiphyllum",
+        nombreComun: "Lirio de la Paz",
+        luz: "Indirecta baja",
+        riego: "Mantener húmedo"
+    },
+    {
+        id: 5,
+        nombre: "Ficus Elastica",
+        nombreComun: "Árbol de Caucho",
+        luz: "Indirecta brillante",
+        riego: "Cada 7-10 días"
+    },
+    {
+        id: 6,
+        nombre: "Zamioculcas Zamiifolia",
+        nombreComun: "ZZ Plant",
+        luz: "Baja a media",
+        riego: "Cada 2-3 semanas"
+    },
+    {
+        id: 7,
+        nombre: "Chamaedorea Elegans",
+        nombreComun: "Palma de Salón",
+        luz: "Indirecta media",
+        riego: "1 vez por semana"
+    },
+    {
+        id: 8,
+        nombre: "Dracaena Marginata",
+        nombreComun: "Drácena",
+        luz: "Indirecta brillante",
+        riego: "Cada 10 días"
+    },
+    {
+        id: 9,
+        nombre: "Aglaonema",
+        nombreComun: "Aglaonema Roja",
+        luz: "Baja a media",
+        riego: "1 vez por semana"
+    },
+    {
+        id: 10,
+        nombre: "Philodendron Hederaceum",
+        nombreComun: "Filodendro Corazón",
+        luz: "Indirecta media",
+        riego: "Cada 7 días"
+    }
+];
